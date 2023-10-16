739be19f0bb062f5cb2027633801384a6605bcc6
B. Piatek <piatekbart@gmail.com>
2023-10-16 19:01:20 +0200

    use spock as testing library

---
+++ src/test/groovy/pl/bpiatek/linkshortener/LinkShortenerApplicationTest.groovy
@@ -0,0 +1,21 @@
+package pl.bpiatek.linkshortener
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+
+/**
+ * Created by Bartosz Piatek on 16/10/2023
+ */
+@SpringBootTest
+class LinkShortenerApplicationTest extends Specification {
+
+    @Autowired
+    ApplicationContext context
+
+    def "should start the application"() {
+        expect:
+        context
+    }
+}
